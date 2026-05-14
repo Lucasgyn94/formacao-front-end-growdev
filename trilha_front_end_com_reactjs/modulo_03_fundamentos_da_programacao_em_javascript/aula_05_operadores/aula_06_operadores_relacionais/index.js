ec3258cfdf1431adcc19b55aff24cbb12db56bc5
Lucas Ferreira Da Silva <needslucas944@gmail.com>
2026-05-13 21:03:50 -0300

    Termino exercício - Operadores Relacionais

---
+++ trilha_front_end_com_reactjs/modulo_03_fundamentos_da_programacao_em_javascript/aula_05_operadores/aula_06_operadores_relacionais/index.js
@@ -0,0 +1,20 @@
+// Operador Relacional - Maior
+const num1 = 5;
+const num2 = 7;
+
+console.log(num2 > num1); // true
+
+// Operador Relacional - Menor
+console.log(num2 < num1); // false
+
+// Operador Relacional - Maior Igual
+console.log(num1 >= 5); // true
+
+// Operador Relacional - Menor Igual
+console.log(num2 <= 8); // true
+
+// Operador Relacional - Extritamente igual
+console.log(num1 === '5'); // false
+
+// Operador Relacional - Extritamente Diferente
+console.log(num1 !== 5); // false
---
+++ trilha_front_end_com_reactjs/modulo_03_fundamentos_da_programacao_em_javascript/aula_05_operadores/aula_07_exercicio_operadores_relacionais/index.js
@@ -0,0 +1,24 @@
+// Utilize operadores relacionais
+
+/*Questão 1. Um cliente tem 17 anos e o produto que ele deseja comprar exige idade mínima de 18 anos. Verifique se ele pode comprar o produto. */
+const questao01 = () => {
+    const idadeCliente = 17;
+    const idadeMinima = 18;
+    const podeComprarProduto = idadeCliente >= idadeMinima ? 
+                                `Você possui ${idadeCliente} anos e está autorizado a comprar o produto!` : 
+                                `Você possui ${idadeCliente} anos e não está autorizado a comprar o produto! \nIdade mínima necessária: ${idadeMinima} anos!`;
+
+    console.log(podeComprarProduto);
+
+}
+//questao01();
+
+/*
+Questão 2. Um cliente deseja realizar uma compra de R$ 300,00, mas seu limite de crédito é de R$ 500,00. Verifique se a compra está dentro do limite de crédito.
+*/
+const valorCompra = 400.00;
+const limiteCredito = 500.00;
+const compraDentroDoLimiteDeCredito = valorCompra <= limiteCredito ?
+                                        `Valor de Compra: ${valorCompra}\nLimite de Crédito ${limiteCredito}\nStatus Compra: Aprovada com sucesso!` :
+                                        `Valor de Compra: ${valorCompra}\nLimite de Crédito ${limiteCredito}\nStatus Compra: Saldo Insuficiente!`;
+console.log(compraDentroDoLimiteDeCredito);
