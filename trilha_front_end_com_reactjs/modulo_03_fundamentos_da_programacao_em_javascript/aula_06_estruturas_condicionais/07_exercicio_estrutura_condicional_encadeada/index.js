6ac670c8a7429d2072f369fec14498bc6556f954
Lucas Ferreira Da Silva <needslucas944@gmail.com>
2026-05-17 23:19:43 -0300

    adicionando método toFixed

--- trilha_front_end_com_reactjs/modulo_03_fundamentos_da_programacao_em_javascript/aula_06_estruturas_condicionais/07_exercicio_estrutura_condicional_encadeada/index.js
+++ trilha_front_end_com_reactjs/modulo_03_fundamentos_da_programacao_em_javascript/aula_06_estruturas_condicionais/07_exercicio_estrutura_condicional_encadeada/index.js
@@ -6,19 +6,21 @@ Questão. Crie um programa que calcule o desconto de um produto com base no valo
 - Se for maior que R$ 500, 20% de desconto;
 */
 
-const precoProduto = 1000;
+const precoProduto = 10;
 const descontoDezPorcento = ((precoProduto * 10) / 100);
 const descontoVintePorCento = ((precoProduto * 20) / 100);
 let precoFinal = 0;
 
 if (precoProduto < 100) {
+    precoFinal = precoProduto;
     console.log("Sem desconto!");
+    console.log(`Valor Final: R$ ${precoFinal.toFixed(2)}`);
 } else if (precoProduto >= 100 && precoProduto <= 500) {
     precoFinal = precoProduto - descontoDezPorcento;
     console.log(`Desconto de 10% aplicado!`);
-    console.log(precoFinal); 
+    console.log(`Valor Final: R$ ${precoFinal.toFixed(2)}`);
 } else {
     precoFinal = precoProduto - descontoVintePorCento;
     console.log(`Desconto de 20% aplicado!`);
-    console.log(precoFinal);
+    console.log(`Valor Final: R$ ${precoFinal.toFixed(2)}`);
 }
