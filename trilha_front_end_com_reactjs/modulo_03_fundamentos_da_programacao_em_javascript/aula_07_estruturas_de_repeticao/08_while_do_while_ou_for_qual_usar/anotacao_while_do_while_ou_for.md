c7f23f903fe9931d60529ab3de9069292182e771
Lucas Ferreira Da Silva <needslucas944@gmail.com>
2026-05-29 23:10:45 -0300

    resolução desafio - while, do while ou for - qual usar?

---
+++ trilha_front_end_com_reactjs/modulo_03_fundamentos_da_programacao_em_javascript/aula_07_estruturas_de_repeticao/08_while_do_while_ou_for_qual_usar/anotacao_while_do_while_ou_for.md
@@ -0,0 +1,8 @@
+# WHILE
+* Se não sabemos o número de iterações que o bloco de código terá, e se a condição precisa ser verificada antes de executar o trecho de código, usamos o __While__.
+
+# DO WHILE
+* Se não sabemos o número de iterações que o bloco de código terá, e se a condição pode ser verificada depois de executar o primeiro trecho de código, usamos o __Do While__.
+
+# FOR
+* Se sabemos o número de iterações que o bloco de código terá, e preferencial se usar o __for__.
---
+++ trilha_front_end_com_reactjs/modulo_03_fundamentos_da_programacao_em_javascript/aula_07_estruturas_de_repeticao/09_exercicio_while_do_while_ou_for_qual_usar/index.js
@@ -0,0 +1,33 @@
+// Questão 1: Imprima os números de 1 a 10, cada um em uma linha.
+const questao01 = () => {
+    for (let i = 1; i <= 10; i++) {
+        console.log(i);
+    }
+}
+
+// Questão 2: Peça ao usuário para inserir palavras. Continue pedindo até que o usuário digite a palavra "sair". Exiba cada palavra digitada.
+const questao02 = () => {
+    let arrayPalavra = [];
+    let palavra = "";
+    do {
+        palavra = prompt("Digite uma palavra ou 'sair' para encerrar o programa");
+        arrayPalavra.push(palavra);
+    } while(palavra != "sair");
+
+    for (let i = 0; i < arrayPalavra.length - 1; i++) {
+        console.log(arrayPalavra[i]);
+    }
+}
+
+// Questão 3: Peça ao usuário que insira a palavra "aceito" para confirmar a leitura dos termos. Continue pedindo até que a palavra "aceito" seja digitada.
+
+let palavra = "";
+
+do {
+    palavra = prompt("Digite 'aceito' para confirmar a leitura dos termos!");
+
+    if (palavra === "aceito") {
+        console.log(`Você concordou com os termos com sucesso!`);
+    }
+
+} while (palavra != "aceito");
