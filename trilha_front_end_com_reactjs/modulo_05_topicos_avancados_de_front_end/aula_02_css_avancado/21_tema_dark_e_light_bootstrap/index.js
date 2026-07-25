89d5ae719cbcd8778c44801cfac5f3fe85d932b9
Lucas Ferreira Da Silva <needslucas944@gmail.com>
2026-07-25 15:24:43 -0300

    criação da estrutura inicial - growflix

--- trilha_front_end_com_reactjs/modulo_05_topicos_avancados_de_front_end/aula_02_css_avancado/21_tema_dark_e_light_bootstrap/index.js
+++ trilha_front_end_com_reactjs/modulo_05_topicos_avancados_de_front_end/aula_02_css_avancado/21_tema_dark_e_light_bootstrap/index.js
@@ -1,20 +1,23 @@
 const btnTema = document.getElementById("btnDark");
-
-let temaAtual = "light";
-const body = document.getElementsByTagName("body");
 const iconeTema = document.getElementById("iconeTema");
-btnTema.addEventListener("click", () => {
-    switch(temaAtual) {
-        case "light":
-            temaAtual = "dark";
-            document.body.setAttribute("data-bs-theme", "dark");
-            iconeTema.setAttribute("class", "bi bi-moon-stars-fill");
-            break;
-        case "dark":
-            temaAtual = "light";
-            document.body.setAttribute("data-bs-theme", "light");
-            iconeTema.setAttribute("class", "bi bi-sun-fill");
-            break;
-        default: "";
+
+let temaAtual = window.localStorage.getItem("tema") || "light";
+
+const aplicarTema = (tema) => {
+    temaAtual = tema;
+
+    document.body.setAttribute("data-bs-theme", tema);
+
+    if (tema === "dark") {
+        iconeTema.className = "bi bi-moon-stars-fill";
+    } else {
+        iconeTema.className = "bi bi-sun-fill";
     }
-});
+    localStorage.setItem("tema", tema);
+}
+
+aplicarTema(temaAtual);
+
+btnTema.addEventListener("click", () => {
+    aplicarTema(temaAtual === "light" ? "dark" : "light");
+})
